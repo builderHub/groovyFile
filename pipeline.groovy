b64bc653cc674199bdc12d6d243b8551e0c66c2a
Imran Ahmad <imran@tm.moneyowl.com.sg>
2025-01-23 09:07:09 +0530

    Adding pipeline grrovy

---
+++ pipeline.groovy
@@ -0,0 +1,48 @@
+pipelineJob("PIPELINEJOB_01") {
+  description("testing seed")
+	
+  definition {
+    cps {
+  
+    script (
+    """
+	pipeline{
+    //global Level
+    agent any
+    
+    
+    //global
+    stages{
+        stage("Stage 01"){
+            //stage level
+            steps{
+                sh '''
+                    ps -ef
+                    uname -a
+                    '''
+            }
+        }
+        
+        stage("Stage 02"){
+            //stage level
+            steps{
+                sh 'echo "hello world"'
+            }
+        }
+    }
+    post{
+        always{
+            echo "This is post always"
+        }
+        failure {
+            echo "This is from failure block"
+        }
+    }
+    
+} 
+
+"""
+) 
+
+  }
+}
